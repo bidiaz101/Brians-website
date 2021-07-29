5d09e5e1d224fdf2801d665e5e11e76a9b5d4a18
brian diaz <bidiaz101@gmail.com>
2021-07-29 15:33:30 -0500

    testing script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+const h2 = document.createElement("h2");
+h2.textContent = "How I started my coding journey";
+document.querySelector("body").appendChild(h2);
+h2.style.color = "#fff";
+h2.style.fontFamily = "Arial";

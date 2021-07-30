376fe26151609199dd96fe405ffd8b79df16c2af
brian diaz <bidiaz101@gmail.com>
2021-07-29 21:00:14 -0500

    style changes and event listener 1 added

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const h2 = document.createElement("h2");
-h2.textContent = "How I started my coding journey";
+h2.textContent = "A brief lil site about me";
 document.querySelector("body").appendChild(h2);
-h2.style.color = "#fff";
-h2.style.fontFamily = "Arial";
+/*h2.style.color = "#fff";
+h2.style.fontFamily = "Arial";*/
+
+function nameClick() {
+    const selfie = document.getElementById("title");
+
+    function clickAlert() {
+        alert('That\'s my name, don\'t wear it out');
+    }
+    
+    selfie.addEventListener('click', clickAlert);
+}
+
+nameClick();
